a4ef7eddd1ea914ed1078666f535fddb2173b981
jeremyevans <jeremy@chiefgeek.io>
2023-11-21 08:01:02 -0500

    added docker command to setup test db

--- temp_install.sh
+++ temp_install.sh
@@ -1,9 +1,1 @@
-docker run --name mysql -e MYSQL_ROOT_PASSWORD=admin -e MYSQL_DATABASE=openams -e MYSQL_USER=admin -e MSQL_PWD=admin -p 3306:3306 -d mysql:latest
-
-docker container exec -it mysql bash
-
-mysql -u root -p
-
-CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+docker run --name postgresdb -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres
